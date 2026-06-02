d8d73e5958268d21c56db20b22a52734b13b168d
balaji7416 <balaji.nani12345@gmail.com>
2026-06-02 17:34:13 +0530

    add silent mode to runCommand

--- backend/src/utils/runCommand.ts
+++ backend/src/utils/runCommand.ts
@@ -1,21 +1,34 @@
 import { spawn } from "child_process";
 
-export const runCommand = (command: string, args: string[], cwd?: string) => {
+type RunOptions = string | { cwd?: string; silent: boolean };
+export const runCommand = (
+  command: string,
+  args: string[],
+  options?: RunOptions,
+) => {
   return new Promise<string>((resolve, reject) => {
     let output = "";
+    let cwd: string | undefined;
+    let silent = false;
 
+    if (typeof options === "string") {
+      cwd = options;
+    } else if (options) {
+      cwd = options.cwd;
+      silent = options.silent;
+    }
     const child = spawn(command, args, {
       cwd,
       shell: true,
     });
 
     child.stdout.on("data", (data) => {
-      process.stdout.write(data);
+      if (!silent) process.stdout.write(data);
       output += data;
     });
 
     child.stderr.on("data", (data) => {
-      process.stderr.write(data);
+      if (!silent) process.stderr.write(data);
       output += data;
     });
 
